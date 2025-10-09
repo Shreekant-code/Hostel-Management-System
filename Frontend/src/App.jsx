53f396c364399ef8e592ea3cdd8e4452cc790674
Shreekant-code <shreekanty06@gmail.com>
2025-10-09 08:19:00 +0530

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+const App=()=>{
+  return(
+    <>
+    
+    <h1 className="text-black-800 text-center text-[50px] pt-90"> We will make hostel management app  admin,warden,students</h1>
+    <p className="text-black pt-5 text-[20px] text-center">  Work in progres...............</p>
+    
+    </>
+  )
+}
+
+export default App;
